503e1617d17d1156d1a872037a59f3f988d04685
Jonathan Castro <neo@neo-mint-one.(none)>
2012-05-01 21:42:40 -0600

    La visualización ahora sera hecha en una pantalla de Cristal Liquido, el ajuste de ciertas constantes para el controlador PID

--- source/DigitalSupply/DigitalSupply.ino
+++ source/DigitalSupply/DigitalSupply.ino
@@ -1,14 +1,29 @@
+/* * LCD RS pin to digital pin 12
+ * LCD Enable pin to digital pin 11
+ * LCD D4 pin to digital pin 7
+ * LCD D5 pin to digital pin 6
+ * LCD D6 pin to digital pin 5
+ * LCD D7 pin to digital pin 4
+ * LCD R/W pin to ground
+ * 10K resistor:
+ * ends to +5V and ground
+ * wiper to LCD VO pin (pin 3)
+*/
+
 #include<avr/interrupt.h>
+#include <LiquidCrystal.h>
 
 #define lecturaSalida A0
-#define referencia    A1
+#define referencia    A5
 
-#define kp  1
-#define ki  1
-#define kd  1
+#define kp  5
+#define ki  0.1
+#define kd  0.1
 
 #define boton  2
-#define pwmCh  3
+#define pwmCh  9
+
+LiquidCrystal lcd(12, 11, 7, 6, 5, 4);
 
 int ovf=0;
 int contador = 0;  //debug
@@ -27,15 +42,17 @@ unsigned char digito0 = 0;
 unsigned char estado = 0;
 
 int error = 0;
-int error_0 = 0;
-int dTerm = 0;
-int iTerm = 0;
-int pTerm = 0;
-int PID = 0;
-int salidaPWM = 0;
+float error_0 = 0;
+float dTerm = 0;
+float iTerm = 0;
+float pTerm = 0;
+float PID = 0;
+float salidaPWM = 0;
 
 void setup(){
+  lcd.begin(16, 2);
   pinMode(boton,INPUT);
+  pinMode(13,INPUT);
   digitalWrite(boton,HIGH);
   analogWrite(pwmCh,255);  //estado inicial del PWM
 
@@ -64,8 +81,7 @@ void loop(){
 
 
 void entradas(){
-  //refCh = analogRead(referencia);
-  //salCh = analogRead(lecturaSalida);
+
   btvisAntes = btVisualizar;
   btVisualizar = digitalRead(boton);
   if (!btvisAntes &&  btVisualizar) btPresionado = 1;
@@ -74,23 +90,38 @@ void entradas(){
 
 
 void MEF(){
-    switch(estado){
-      case 0:
-        convertBCD(salCh);
-        if(btPresionado) estado = 1;
-      break;
-      
-      case 1:
-        convertBCD(refCh);
-        if(btPresionado) estado = 0;
-        
-      break;
-      
-      default:
-      break;      
-    }
 }
 
+
+
+void salidas(){
+  float refDisp = 0;
+  float salDisp = 0;
+  refDisp = refCh;
+  salDisp = salCh;
+  refDisp = refDisp*12/1023;
+  salDisp = salDisp*12/1023;
+  lcd.clear();
+  lcd.print("Ref: ");
+  if(refDisp > 10)   lcd.setCursor(11, 0);
+  else   lcd.setCursor(12, 0);
+  lcd.print(refDisp, DEC);
+  lcd.setCursor(0,1);
+  lcd.print("Salida: ");
+  if(salDisp > 10)   lcd.setCursor(11, 1);
+  else   lcd.setCursor(12, 1);
+  lcd.print(salDisp, DEC);
+  Serial.print(refCh, DEC);
+  Serial.print(", ");
+  Serial.println(salCh, DEC);
+  Serial.print("Error: ");
+  Serial.println(error, DEC);
+  delay(1000);
+  
+  
+
+  
+}
 
 void convertBCD(unsigned int x){
         digito0 = x % 10;
@@ -98,16 +129,7 @@ void convertBCD(unsigned int x){
         digito1 = x%10;
         x=x/10;
         digito2 = x%1000;
-        
 }
-
-void salidas(){
-//
-//Serial.print(digito1, DEC);
-//Serial.println(digito0, DEC);
-delay(10);
-}
-
 
 //ISR
 ISR(TIMER2_OVF_vect){
@@ -135,10 +157,11 @@ ISR(TIMER2_OVF_vect){
     if(salidaPWM < 102) salidaPWM = 100;
     else if(salidaPWM > 921) salidaPWM = 920;
     
-    analogWrite(pwmCh, 255 - salidaPWM/4);
-   
+    analogWrite(pwmCh, 255 - salidaPWM/4);   
   }
-  
-  
 
 }
+
+
+
+
